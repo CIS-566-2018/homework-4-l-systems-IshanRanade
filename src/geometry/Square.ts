a5e339d136ca8583cd79133c0ab4b8f6ddab0edd
Ishan Ranade <ranadeishan@gmail.com>
2018-02-19 13:53:07 -0500

    add colors to vbo

--- src/geometry/Square.ts
+++ src/geometry/Square.ts
@@ -6,6 +6,7 @@ class Square extends Drawable {
   indices: Uint32Array;
   positions: Float32Array;
   normals: Float32Array;
+  colors: Float32Array;
   center: vec4;
 
   constructor(center: vec3) {
@@ -15,20 +16,26 @@ class Square extends Drawable {
 
   create() {
 
-  this.indices = new Uint32Array([0, 1, 2,
-                                  0, 2, 3]);
-  this.normals = new Float32Array([0, 0, 1, 0,
-                                   0, 0, 1, 0,
-                                   0, 0, 1, 0,
-                                   0, 0, 1, 0]);
-  this.positions = new Float32Array([-1, -1, 0, 1,
-                                     1, -1, 0, 1,
-                                     1, 1, 0, 1,
-                                     -1, 1, 0, 1]);
+    this.indices = new Uint32Array([0, 1, 2,
+                                    0, 2, 3]);
+    this.normals = new Float32Array([0, 0, 1, 0,
+                                    0, 0, 1, 0,
+                                    0, 0, 1, 0,
+                                    0, 0, 1, 0]);
+    this.positions = new Float32Array([-1, -1, 0, 1,
+                                    1, -1, 0, 1,
+                                    1, 1, 0, 1,
+                                    -1, 1, 0, 1]);
+    this.colors = new Float32Array([0, 1, 1, 1,
+                                    0, 1, 0, 1,
+                                    0, 1, 0, 1,
+                                    0, 1, 0, 1]);
+
 
     this.generateIdx();
     this.generatePos();
     this.generateNor();
+    this.generateCol();
 
     this.count = this.indices.length;
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufIdx);
@@ -39,6 +46,9 @@ class Square extends Drawable {
 
     gl.bindBuffer(gl.ARRAY_BUFFER, this.bufPos);
     gl.bufferData(gl.ARRAY_BUFFER, this.positions, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.bufCol);
+    gl.bufferData(gl.ARRAY_BUFFER, this.colors, gl.STATIC_DRAW);
 
     console.log(`Created square`);
   }
